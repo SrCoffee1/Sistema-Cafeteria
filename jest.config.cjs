fe69cbe7fb34035e08eb14de3238083103c9689d
SrCoffee1 <joao.v.carvalho8@ba.estudante.senai.br>
2025-04-27 13:40:02 -0300

    FOI
    FOI

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,4 @@
-//jest.config.cjs
+//jest.config.cjs.
 module.exports = {
   testEnvironment: 'jsdom', // Configura o ambiente para jsdom
   testTimeout: 15000, // Define 15 segundos como timeout padrão para todos os testes
